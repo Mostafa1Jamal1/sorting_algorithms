34135287521a88f9a106f1819c21b36b622fd914
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-20 05:15:57 +0300

    refactor: 0-bubble_sort.c and add comment

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,20 +9,25 @@
 
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i, j, swaps;
 	int tmp;
 
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
+		swaps = 0;
 		for (j = 0; j < size - 1 - i; j++)
 		{
+			printf("i = %ld , j = %ld\n", i, j);
 			if (array[j] > array[j + 1])
-			{
+			{	/* Swap the elements and add 1 to swaps */
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 				print_array(array, size);
+				swaps++;
 			}
 		}
+		if (swaps == 0)
+			return;
 	}
 }
