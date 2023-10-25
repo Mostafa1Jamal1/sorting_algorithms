10cee2525245bfc45f074a9d9011185827c2bc4c
Mostafa_Jamal <mostafa.jamal.mjm@gmail.com>
2023-10-25 07:15:20 +0300

    task3 and modify README.md and sort.h

---
+++ 3-quick_sort.c
@@ -0,0 +1,52 @@
+#include "sort.h"
+/**
+ *
+ */
+void recursive_quick_sort(int *array, size_t size, long start, long end)
+{
+	long i, j;
+	int tmp, pivot;
+	bool isbig = false;
+	/* Base condition */
+        if (start >= end)
+                return;
+	/* Partitioning */
+	pivot = array[end];
+	i = start;
+	for (j = start; j < end; j++)
+	{
+		if (array[j] < pivot && !isbig)
+		{
+			i++;
+			continue;
+		}
+		if (array[j] < pivot)
+		{	/* Swap array[i] with array[j] */
+			tmp = array[i];
+			array[i] = array[j];
+			array[j] = tmp;
+			i++;
+			print_array(array, size);
+		}
+		isbig = true;
+	}
+	if (!isbig)
+		recursive_quick_sort(array, size, start, end - 1);
+	else
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+		print_array(array, size);
+		recursive_quick_sort(array, size, start, i - 1);
+		recursive_quick_sort(array, size, i + 1, end);
+	}
+}
+
+void quick_sort(int *array, size_t size)
+{
+	long start = 0;
+	long end = size - 1;
+
+	recursive_quick_sort(array, size, start, end);
+}
--- README.md
+++ README.md
@@ -46,3 +46,15 @@ Any file that does't have a description -if there is any- is propably for testin
 - in the average case
 - in the worst case
 
+
+`3-quick_sort.c` -> a source code file for a function that sorts an array of integers in ascending order using the Quick sort algorithm
+- Prototype: `void quick_sort(int *array, size_t size);`
+- You must implement the Lomuto partition scheme.
+- The pivot should always be the last element of the partition being sorted.
+- You’re expected to print the array after each time you swap two elements
+
+`3-O` the big O notations of the time complexity of the Quick sort algorithm, with 1 notation per line:
+- in the best case
+- in the average case
+- in the worst case
+
